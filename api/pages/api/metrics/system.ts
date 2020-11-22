277c0124451b64b327724f5ebe767c873312b71b
Anson <git@lichtf.us>
2020-11-21 17:58:19 -0700

    feat: add support for truncating table rows

--- api/pages/api/metrics/system.ts
+++ api/pages/api/metrics/system.ts
@@ -6,6 +6,17 @@ import { generateSystemPayload } from '../../../lib/metrics/generateSystemPayloa
 
 const { GOOGLE_SHEETS_API_KEY, GOOGLE_SHEET_ID } = process.env;
 
+const MAX_TABLE_ROWS = 300;
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+async function getAuthToken() {
+  const auth = new google.auth.GoogleAuth({
+    scopes: SCOPES
+  });
+  const authToken = await auth.getClient();
+  return authToken;
+}
+
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
@@ -21,12 +32,38 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const sheets = google.sheets({
       version: 'v4',
-      auth: GOOGLE_SHEETS_API_KEY,
+      auth: await getAuthToken(),
     });
     const { data } = await sheets.spreadsheets.values.get({
       spreadsheetId: GOOGLE_SHEET_ID,
       range: 'Metrics!A1:G',
     });
+
+    // Truncate more than max number of rows
+    const rowCount = (data.values || []).length;
+    if (rowCount > MAX_TABLE_ROWS) {
+      const startOffset = 1;
+      const rowsToDelete = rowCount - MAX_TABLE_ROWS;
+
+      await sheets.spreadsheets.batchUpdate({
+        spreadsheetId: GOOGLE_SHEET_ID,
+        requestBody: {
+          "requests":
+          [
+            {
+              "deleteDimension": {
+                "range": {
+                  "sheetId": 0,
+                  "dimension": "ROWS",
+                  "startIndex": startOffset, // Start at second row, first row is column titles
+                  "endIndex": rowsToDelete + startOffset
+                }
+              }
+            },
+          ]
+        }
+      });
+    }
 
     const rows = generateSystemPayload(data);
 
